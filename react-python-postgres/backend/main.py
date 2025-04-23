2bd9dc727d1eaee43536e82de3941f0ac168ece5
ed <ejhall78@gmail.com>
2025-04-23 14:27:17 +0100

    add reset function and handle CORS

--- react-python-postgres/backend/main.py
+++ react-python-postgres/backend/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 clicks = 0
 
@@ -11,12 +24,18 @@ def read_root():
 
 @app.get("/getclicks")
 def get_clicks():
-    return clicks
+    return {clicks}
 
 @app.get("/incrementclicks")
 def get_clicks():
     global clicks
     clicks += 1
+    return clicks
+
+@app.get("/resetclicks")
+def get_clicks():
+    global clicks
+    clicks = 0
     return clicks
 
 # TODO: 
