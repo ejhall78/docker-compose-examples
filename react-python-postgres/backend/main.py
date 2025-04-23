695fa7fa441cbfa194bef95cbf54ac9f9aea0b9a
ed <ejhall78@gmail.com>
2025-04-23 11:00:53 +0100

    add simple backend
    implement simple FastAPI routing and increment a variable

--- react-python-postgres/backend/main.py
+++ react-python-postgres/backend/main.py
@@ -1,3 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+clicks = 0
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/getclicks")
+def get_clicks():
+    return clicks
+
+@app.get("/incrementclicks")
+def get_clicks():
+    global clicks
+    clicks += 1
+    return clicks
+
 # TODO: 
-#    - Implement FastAPI for routing
 #    - Implement PostgreSQL database connection and relevant functions
